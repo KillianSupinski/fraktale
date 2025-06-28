a056ac15f705d0149a3b829fbd423285a13bd06c
Killian Supinski <ksupinsk@f2r6s7.clusters.42paris.fr>
2025-06-28 17:14:54 +0200

    ajout debut fonction math

--- srcs/math.c
+++ srcs/math.c
@@ -6,8 +6,24 @@
 /*   By: ksupinsk <ksupinsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 12:07:37 by ksupinsk          #+#    #+#             */
-/*   Updated: 2025/06/27 12:07:47 by ksupinsk         ###   ########.fr       */
+/*   Updated: 2025/06/28 17:00:16 by ksupinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_fractol.h"
+
+int	c_new(int r, int i)
+{
+
+}
+
+int	c_add(int a, int b)
+{
+	return (a + b);
+}
+int	c_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
